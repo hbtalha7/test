e3894796eae812a980d71a6e83ae400d9de40d02
MOHAMMED TALHA <atalhahb@gmail.com>
2021-07-12 20:00:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Homescreen from "./screens/Homescreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header>
+          <img src="./images/logo.png" alt="Logo"></img>
+        </header>
+        <main>
+          <Switch>
+            <Route to="/" component={Homescreen}></Route>
+          </Switch>
+        </main>
+        <footer>reserved all copyright</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
